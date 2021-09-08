9582fe311733b00dff3129666b050199a32bf79b
samw2703 <samw2703@gmail.com>
2021-09-08 13:39:58 +0100

    Create abstract real model providers

---
+++ ReadModelFactory/ReadModelProvider.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace ReadModelFactory
+{
+	public abstract class ReadModelProvider<TReadModel, TArgs>
+	{
+		public abstract Task<TReadModel> Get(TArgs args);
+	}
+
+	public abstract class ReadModelProvider<TReadModel> : ReadModelProvider<TReadModel, NoReadModelArgs>
+	{
+		public override async Task<TReadModel> Get(NoReadModelArgs args)
+			=> await Get();
+
+		public abstract Task<TReadModel> Get();
+	}
+}
