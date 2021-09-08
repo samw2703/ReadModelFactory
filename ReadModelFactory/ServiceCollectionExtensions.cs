2ba52d608f9234239b3a99a552749b66227a27c9
samw2703 <samw2703@gmail.com>
2021-09-08 14:32:30 +0100

    add ServiceCollection extension for wiring up purposes

--- ReadModelFactory/ServiceCollectionExtensions.cs
+++ ReadModelFactory/ServiceCollectionExtensions.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace ReadModelFactory
 {
 	public static class ServiceCollectionExtensions
 	{
-		public static void AddReadModelFactory(this IServiceCollection serviceCollection)
+		public static void AddReadModelFactory(this IServiceCollection serviceCollection, params Assembly[] assemblies)
 		{
 			serviceCollection.AddSingleton<IReadModelFactory, ReadModelFactory>();
+			AddReadModelCatalogue(serviceCollection, assemblies);
+		}
+
+		private static void AddReadModelCatalogue(IServiceCollection serviceCollection, Assembly[] assemblies)
+		{
+			var catalogueItems = new ReadModelCatalogueItemsProvider(new AssemblyTypesProvider())
+				.Get(assemblies);
+			foreach (var catalogueItem in catalogueItems)
+				serviceCollection.AddTransient(catalogueItem.ProviderType);
+
+			serviceCollection.AddSingleton(new ReadModelCatalogue(catalogueItems));
 		}
 	}
 }
